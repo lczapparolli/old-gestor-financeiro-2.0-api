fb1e71ac021d581d904125346e07f3779d0c85b1
Luis Carlos <lczapparolli@gmail.com>
2018-09-04 00:06:20 -0300

    Downgrading Gulp to work with nodemon
    - gulp-nodemon plugin only works with Gulp 3

--- api/gulpfile.js
+++ api/gulpfile.js
@@ -3,38 +3,30 @@ const MODULES_BIN = 'node_modules/.bin/';
 const gulp = require('gulp');
 const exec = require('child_process').exec;
 const del = require('del');
+const nodemon = require('gulp-nodemon');
 
 //Test tasks-----------------------------
-const testEnv = (done) => {
+gulp.task('test:env', (done) => {
   process.env.NODE_ENV = 'test';
   done();
-};
-testEnv.displayName = 'Set test environment';
+});
 //Test tasks-----------------------------
 
 
 //DB Tasks-------------------------------
-const dbMigrate = (done) => {
+gulp.task('db:migrate', (done) => {
   exec(MODULES_BIN + '/sequelize db:migrate', (err, stdout, stderr) => {
     done(err);
   });
-};
-dbMigrate.displayName = 'Database migration';
+});
 
-const dbCleanup = () => {
+gulp.task('db:cleanup', () => {
   var env = process.env.NODE_ENV || 'development';
   return del('db/' + env + '.sqlite3');
-};
-dbCleanup.displayName = 'Clean database';
+});
 //DB Tasks-------------------------------
 
 
-//Final tasks----------------------------
-const test = gulp.series(testEnv, dbCleanup, dbMigrate);
-test.description = 'Check database scripts and execute unit tests';
-//Final tasks----------------------------
-
-
 //Exports to Gulp------------------------
-gulp.task('test', test);
+gulp.task('test', ['test:env', 'db:migrate', 'db:cleanup']);
 //Exports to Gulp------------------------
