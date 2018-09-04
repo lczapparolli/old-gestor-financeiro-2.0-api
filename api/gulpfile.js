ac7d5c0a45166e9df65cdeeaeffcde536f7467bc
Luis Carlos <lczapparolli@gmail.com>
2018-09-03 21:58:04 -0300

    Creating Gulp tasks:
    - Main 'test' task
    - Deleting test database
    - Setting test envitonment
    - Running Sequelize migrations

--- api/gulpfile.js
+++ api/gulpfile.js
@@ -1,6 +1,40 @@
-var gulp = require('gulp');
+const MODULES_BIN = 'node_modules/.bin/';
 
-gulp.task('default', function(done) {
-  console.log('Gulp is running');
+const gulp = require('gulp');
+const exec = require('child_process').exec;
+const del = require('del');
+
+//Test tasks-----------------------------
+const testEnv = (done) => {
+  process.env.NODE_ENV = 'test';
   done();
-});
+};
+testEnv.displayName = 'Set test environment';
+//Test tasks-----------------------------
+
+
+//DB Tasks-------------------------------
+const dbMigrate = (done) => {
+  exec(MODULES_BIN + '/sequelize db:migrate', (err, stdout, stderr) => {
+    done(err);
+  });
+};
+dbMigrate.displayName = 'Database migration';
+
+const dbCleanup = () => {
+  var env = process.env.NODE_ENV || 'development';
+  return del('db/' + env + '.sqlite3');
+};
+dbCleanup.displayName = 'Clean database';
+//DB Tasks-------------------------------
+
+
+//Final tasks----------------------------
+const test = gulp.series(testEnv, dbCleanup, dbMigrate);
+test.description = 'Check database scripts and execute unit tests';
+//Final tasks----------------------------
+
+
+//Exports to Gulp------------------------
+gulp.task('test', test);
+//Exports to Gulp------------------------
