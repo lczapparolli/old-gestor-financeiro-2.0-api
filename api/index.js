f81976ebf6d228e632281ff5e6354d3c161a8cf0
Luis Carlos <lczapparolli@gmail.com>
2018-09-18 22:03:25 -0300

    Adding logger
    - Adding Morgan to log requests

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const parser = require('body-parser');
+const morgan = require('morgan');
 const UserController = require('./app/controllers/userController');
 const app = express();
+const env = process.env.NODE_ENV || 'development';
 
 app.use(parser.json());
+if (env === 'production') {
+    app.use(morgan('combined'));
+} else if (env === 'development') {
+    app.use(morgan('dev'));
+}
 
 app.get('/', (request, response) => {
     response.send('gestor-financeiro-2.0');
